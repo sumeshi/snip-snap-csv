58c3ce8872fc5da0c519459d44b1c2499802c595
S.Nakano <35072092+sumeshi@users.noreply.github.com>
2024-06-26 15:06:39 +0000

    doc: docstring

--- src/sscsv/controllers/DataFrameController.py
+++ src/sscsv/controllers/DataFrameController.py
@@ -12,11 +12,13 @@ class DataFrameController(object):
     
     # initialize methods
     def load(self, path):
+        """[initializer] Loads the specified CSV file."""
         self.df = CsvController(path=path).get_dataframe()
         return self
 
     # chainable methods
     def select(self, columns: str) -> None:
+        """[chainable] Displays the specified columns."""
         def parse_columns(headers: list[str], columns: tuple[str]):
             if type(columns) is tuple:
                 columns = ",".join(columns)
@@ -40,27 +42,33 @@ class DataFrameController(object):
         return self
     
     def isin(self, colname: str, values: list):
+        """[chainable] Displays rows that contain the specified values."""
         self.df = self.df.filter(pl.col(colname).is_in(values))
         return self
     
     def contains(self, colname: str, regex: str):
+        """[chainable] Displays rows that contain the specified string."""
         regex = regex if type(regex) is str else str(regex)
         self.df = self.df.filter(pl.col(colname).str.contains(regex))
         return self
 
     def head(self, number: int = 5):
+        """[chainable] Displays the first specified number of rows of the data."""
         self.df = self.df.head(number)
         return self
 
     def tail(self, number: int = 5):
+        """[chainable] Displays the last specified number of rows of the data."""
         self.df = self.df.tail(number)
         return self
     
     def sort(self, columns: str, desc: bool = False):
+        """[chainable] Sorts the data by the values of the specified column."""
         self.df = self.df.sort(columns, descending=desc)
         return self
     
     def changetz(self, colname: str, timezone_from: str = "UTC", timezone_to: str = "Asia/Tokyo", new_colname: str = None):
+        """[chainable] Changes the timezone of the specified date column."""
         new_colname = new_colname if new_colname else colname
         self.df.with_columns(pl.col(colname).dt.replace_time_zone(timezone_from))
         self.df = self.df.with_columns(
@@ -70,6 +78,7 @@ class DataFrameController(object):
     
     # finalize methods
     def headers(self, plain: bool = False):
+        """[finalizer] Displays the column names of the data."""
         if plain:
             print(",".join([f"\"{c}\"" for c in self.df.columns]))
         else:
@@ -78,16 +87,20 @@ class DataFrameController(object):
                 values=[[str(i).zfill(2), c] for i, c in enumerate(self.df.columns)]
             )
     
-    def stat(self):
+    def stats(self):
+        """[finalizer] Displays the statistical information of the data."""
         print(self.df.describe())       
 
     def showquery(self):
+        """[finalizer] Displays the data processing query."""
         print(self.df)
 
     def show(self):
+        """[finalizer] Outputs the processing results to the standard output."""
         self.df.collect().write_csv(sys.stdout)
     
     def dump(self, path: str = None):
+        """[finalizer] Outputs the processing results to a CSV file."""
         def autoname():
             now = datetime.now().strftime('%Y%m%d-%H%M%S')
             query = self.df.explain(optimized=False).splitlines()[0]
@@ -101,7 +114,7 @@ class DataFrameController(object):
         path = path if path else autoname()
         self.df.collect().write_csv(path)
     
-    def __str__(self):
-        if self.df is not None:
-            print(self.df.collect())
-        return ''
+    # def __str__(self):
+    #     if self.df is not None:
+    #         print(self.df.collect())
+    #     return ''
